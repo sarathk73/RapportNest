30a9c2b7211c1c9638793c5354248a7371e50da8
SARATH K <142706927+sarathk73@users.noreply.github.com>
2024-03-11 19:57:16 +0530

    Add UsersList component for displaying user items

---
+++ frontend/src/user/components/UsersList.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import UserItem from './UserItem';
+import './UsersList.css';
+
+const UsersList = props => {
+  if (props.items.length === 0) {
+    return (
+      <div className="center">
+        <h2>No users found.</h2>
+      </div>
+    );
+  }
+
+  return (
+    <ul className="users-list">
+      {props.items.map(user => (
+        <UserItem
+          key={user.id}
+          id={user.id}
+          image={user.image}
+          name={user.name}
+          placeCount={user.places}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default UsersList;
