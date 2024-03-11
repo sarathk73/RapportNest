74331221bc356f0a59693391d496f1e556a8ecde
SARATH K <142706927+sarathk73@users.noreply.github.com>
2024-03-11 20:02:04 +0530

    Update Users component to use local sarath.jpeg image and update user name

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
+import UsersList from '../components/UsersList';
+import sarathImage from '../../sarath.jpeg'; // This path should correctly point to src/sarath.jpeg
+
 const Users = () => {
-  return <h2>Users Works!</h2>;
+  const USERS = [
+    {
+      id: 'u1',
+      name: 'Sarath K',
+      image:sarathImage,
+      places: 3
+    }
+  ];
+
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
