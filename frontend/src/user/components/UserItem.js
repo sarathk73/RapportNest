bdc012b92c1f2bb4c2622e488b395a023e7908b5
SARATH K <142706927+sarathk73@users.noreply.github.com>
2024-03-11 22:55:58 +0530

    Wrap UserItem content with Card component for consistent styling

--- frontend/src/user/components/UserItem.js
+++ frontend/src/user/components/UserItem.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import Avatar from '../../shared/components/UIElements/Avatar';
+import Card from '../../shared/components/UIElements/Card';
 
 import './UserItem.css';
 
@@ -9,17 +10,19 @@ const UserItem = props => {
   return (
     <li className="user-item">
       <div className="user-item__content">
-         <Link to={`/${props.id}/contacts`}>
-            <div className="user-item__image">
-            <Avatar image={props.image} alt={props.name} />
-            </div>
-            <div className="user-item__info">
-            <h2>{props.name}</h2>
-            <h3>
-                {props.contactCount} {props.contactCount === 1 ? 'Contact' : 'Contacts'}
-            </h3>
-            </div>
-        </Link>
+        <Card>
+            <Link to={`/${props.id}/contacts`}>
+                <div className="user-item__image">
+                <Avatar image={props.image} alt={props.name} />
+                </div>
+                <div className="user-item__info">
+                <h2>{props.name}</h2>
+                <h3>
+                    {props.contactCount} {props.contactCount === 1 ? 'Contact' : 'Contacts'}
+                </h3>
+                </div>
+            </Link>
+        </Card>
       </div>
     </li>
   );
