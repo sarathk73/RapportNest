92b9eafa95c9c7bd295253026f72982570756a81
SARATH K <142706927+sarathk73@users.noreply.github.com>
2024-03-11 19:59:35 +0530

    Add UserItem component to display individual user details

---
+++ frontend/src/user/components/UserItem.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import './UserItem.css';
+
+const UserItem = props => {
+  return (
+    <li className="user-item">
+      <div className="user-item__content">
+        <div className="user-item__image">
+          <img src={props.image} alt={props.name} />
+        </div>
+        <div className="user-item__info">
+          <h2>{props.name}</h2>
+          <h3>
+            {props.placeCount} {props.placeCount === 1 ? 'Place' : 'Places'}
+          </h3>
+        </div>
+      </div>
+    </li>
+  );
+};
+
+export default UserItem;
