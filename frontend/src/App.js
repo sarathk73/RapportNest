86a479f88409bab96b8c49cbca37ef6ff26c341b
SARATH K <142706927+sarathk73@users.noreply.github.com>
2024-03-12 19:00:06 +0530

    Add navigation components to the main app layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,19 +8,23 @@ import {
 
 import Users from './user/pages/Users';
 import NewContact from './contacts/pages/NewContact';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
 
 const App = () => {
   return (
     <Router>
-      <Switch>
-        <Route path="/" exact>
-          <Users />
-        </Route>
-        <Route path="/contacts/new" exact>
-          <NewContact />
-        </Route>
-        <Redirect to="/" />
-      </Switch>
+      <main>
+        <MainNavigation />
+        <Switch>
+          <Route path="/" exact>
+            <Users />
+          </Route>
+          <Route path="/contacts/new" exact>
+            <NewContact />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </main>
     </Router>
   );
 };
