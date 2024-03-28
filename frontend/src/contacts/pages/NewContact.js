8536ab20464c19d4293204ed33f2816098b1c17c
SARATH K <142706927+sarathk73@users.noreply.github.com>
2024-03-28 22:01:40 +0530

    Add form submission handling and address field to NewContact form

--- frontend/src/contacts/pages/NewContact.js
+++ frontend/src/contacts/pages/NewContact.js
@@ -56,8 +56,13 @@ const NewContact = () => {
     });
   }, []);
 
+  const placeSubmitHandler = event => {
+    event.preventDefault();
+    console.log(formState.inputs); // send this to the backend!
+  };
+
   return (
-    <form className="contact-form">
+    <form className="contact-form" onSubmit={placeSubmitHandler}>
       <Input
         id="title"
         element="input"
@@ -73,6 +78,14 @@ const NewContact = () => {
         label="Description"
         validators={[VALIDATOR_MINLENGTH(5)]}
         errorText="Please enter a valid description (at least 5 characters)."
+        onInput={inputHandler}
+      />
+      <Input
+        id="address"
+        element="input"
+        label="Address"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="Please enter a valid address."
         onInput={inputHandler}
       />
       <Button type="submit" disabled={!formState.isValid}>
