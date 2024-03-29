b7b109282f9cc2c02ebc25521bdd20f0898b6555
SARATH K <142706927+sarathk73@users.noreply.github.com>
2024-03-29 12:15:25 +0530

    Refactor UpdateContact component to use useForm hook for form updates

--- frontend/src/contacts/pages/UpdateContact.js
+++ frontend/src/contacts/pages/UpdateContact.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+
 import sarathImage from '../../sarath.jpeg'; // This path should correctly point to src/sarath.jpeg
+
 import Input from '../../shared/components/FormElements/Input';
 import Button from '../../shared/components/FormElements/Button';
 import {
   VALIDATOR_REQUIRE,
   VALIDATOR_MINLENGTH
 } from '../../shared/util/validators';
+import { useForm } from '../../shared/hooks/form-hook';
+import './NewContact.css';
 
 const DUMMY_CONTACTS = [
     {
@@ -19,10 +23,34 @@ const DUMMY_CONTACTS = [
     }
   ];
 
+
 const UpdateContact = () => {
   const contactId = useParams().contactId;
 
   const identifiedContact = DUMMY_CONTACTS.find(p => p.id === contactId);
+
+  const [formState, inputHandler] = useForm(
+    {
+      title: {
+        value: identifiedContact.title,
+        isValid: true
+      },
+      description: {
+        value: identifiedContact.description,
+        isValid: true
+      },
+      phone: {
+        value: identifiedContact.phone,
+        isValid: true
+      }
+    },
+    true
+  );
+
+  const contactUpdateSubmitHandler = event => {
+    event.preventDefault();
+    console.log(formState.inputs);
+  };
 
   if (!identifiedContact) {
     return (
@@ -33,17 +61,17 @@ const UpdateContact = () => {
   }
 
   return (
-    <form>
+    <form className="contact-form" onSubmit={contactUpdateSubmitHandler}>
       <Input
         id="title"
         element="input"
         type="text"
         label="Name"
         validators={[VALIDATOR_REQUIRE()]}
-        errorText="Please enter a valid title."
-        onInput={() => {}}
-        value={identifiedContact.title}
-        valid={true}
+        errorText="Please enter a valid name."
+        onInput={inputHandler}
+        initialValue={formState.inputs.title.value}
+        initialValid={formState.inputs.title.isValid}
       />
       <Input
         id="description"
@@ -51,21 +79,21 @@ const UpdateContact = () => {
         label="Description"
         validators={[VALIDATOR_MINLENGTH(5)]}
         errorText="Please enter a valid description (min. 5 characters)."
-        onInput={() => {}}
-        value={identifiedContact.description}
-        valid={true}
+        onInput={inputHandler}
+        initialValue={formState.inputs.description.value}
+        initialValid={formState.inputs.description.isValid}
       />
       <Input
         id="phone"
         element="input"
         label="Phone Number"
         validators={[VALIDATOR_MINLENGTH(10)]}
-        errorText="Please enter a valid phone number (at least 10)."
-        onInput={() => {}}
-        value={identifiedContact.phone}
-        valid={true}
+        errorText="Please enter a valid Number."
+        onInput={inputHandler}
+        initialValue={formState.inputs.phone.value}
+        initialValid={formState.inputs.phone.isValid}
       />
-      <Button type="submit" disabled={true}>
+      <Button type="submit" disabled={!formState.isValid}>
         UPDATE CONTACT
       </Button>
     </form>
