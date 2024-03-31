5cdfc50126536bd44cc3ed55716310b4486a41cb
SARATH K <142706927+sarathk73@users.noreply.github.com>
2024-03-31 11:12:36 +0530

    Adding Auth Context for App-wide State Management

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import {
   BrowserRouter as Router,
   Route,
@@ -12,32 +12,47 @@ import UserContacts from './contacts/pages/UserContact';
 import UpdateContact from './contacts/pages/UpdateContact';
 import Auth from './user/pages/Auth';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
+import { AuthContext } from './shared/context/auth-context';
 
 const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const login = useCallback(() => {
+    setIsLoggedIn(true);
+  }, []);
+
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+  }, []);
+
   return (
-    <Router>
-      <main>
+    <AuthContext.Provider
+      value={{ isLoggedIn: isLoggedIn, login: login, logout: logout }}
+    >
+      <Router>
         <MainNavigation />
-        <Switch>
-          <Route path="/" exact>
-            <Users />
-          </Route>
-          <Route path="/:userId/contacts" exact>
-            <UserContacts />
-          </Route>
-          <Route path="/contacts/new" exact>
-            <NewContact />
-          </Route>
-          <Route path="/contacts/:contactId">
-            <UpdateContact />
-          </Route>
-          <Route path="/auth">
-            <Auth />
-          </Route>
-          <Redirect to="/" />
-        </Switch>
-      </main>
-    </Router>
+        <main>
+          <Switch>
+            <Route path="/" exact>
+              <Users />
+            </Route>
+            <Route path="/:userId/contacts" exact>
+              <UserContacts />
+            </Route>
+            <Route path="/contacts/new" exact>
+              <NewContact/>
+            </Route>
+            <Route path="/contacts/:contactId">
+              <UpdateContact />
+            </Route>
+            <Route path="/auth">
+              <Auth />
+            </Route>
+            <Redirect to="/" />
+          </Switch>
+        </main>
+      </Router>
+    </AuthContext.Provider>
   );
 };
 
