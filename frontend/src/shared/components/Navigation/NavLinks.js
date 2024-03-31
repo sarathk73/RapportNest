24bbbc858cc5c9460ba20dbfcdedf0b2a298c6c3
SARATH K <142706927+sarathk73@users.noreply.github.com>
2024-03-31 20:47:10 +0530

    Updated NavLinks component to show links based on authentication status

--- frontend/src/shared/components/Navigation/NavLinks.js
+++ frontend/src/shared/components/Navigation/NavLinks.js
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, { useContext }  from 'react';
 import { NavLink } from 'react-router-dom';
 
+import { AuthContext } from '../../context/auth-context';
 import './NavLinks.css';
 
 const NavLinks = props => {
+  const auth = useContext(AuthContext);
+
   return <ul className="nav-links">
     <li>
       <NavLink to="/" exact>ALL USERS</NavLink>
     </li>
-    <li>
-      <NavLink to="/u1/contacts">MY CONTACTS</NavLink>
-    </li>
-    <li>
-      <NavLink to="/contacts/new">ADD CONTACT</NavLink>
-    </li>
-    <li>
-      <NavLink to="/auth">AUTHENTICATE</NavLink>
-    </li>
+    {auth.isLoggedIn && (
+      <li>
+        <NavLink to="/u1/contacts">MY CONTACTS</NavLink>
+      </li>
+    )}
+    {auth.isLoggedIn && (
+      <li>
+        <NavLink to="/contacts/new">ADD CONTACT</NavLink>
+      </li>
+    )}
+    {!auth.isLoggedIn && (
+      <li>
+        <NavLink to="/auth">AUTHENTICATE</NavLink>
+      </li>
+    )}
   </ul>
 };
 
