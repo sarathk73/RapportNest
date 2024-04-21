5caf5731ca2868be0805b4f8d9ca6ece74806848
SARATH K <142706927+sarathk73@users.noreply.github.com>
2024-04-21 11:23:38 +0530

    Add Contact Specific Routes

--- backend/routes/contacts-routes.js
+++ backend/routes/contacts-routes.js
@@ -2,9 +2,23 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  console.log('GET Request in Contacts');
-  res.json({message: 'It works!'});
-});
+const DUMMY_CONTACTS = [
+    {
+      id: 'p1',
+      title: 'Sarath K',
+      description: 'Upcoming Associate Engineer !',
+      imageUrl: 'https://i.ibb.co/3yK7hXt/Whats-App-Image-2024-03-28-at-12-08-34-PM.jpg',
+      phone: '7306162306',
+      creator: 'u1'
+    }
+];
+
+router.get('/:pid', (req, res, next) => {
+    const contactId = req.params.pid; // { pid: 'p1' }
+    const contact = DUMMY_CONTACTS.find(p => {
+      return p.id === contactId;
+    });
+    res.json({contact}); // => { contact } => { contact: contact }
+  });
 
 module.exports = router;
