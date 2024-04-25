3a72c65855d553ff9d1982990443751937bc0a91
SARATH K <142706927+sarathk73@users.noreply.github.com>
2024-04-25 09:01:03 +0530

    Validating User Routes

--- backend/controllers/users-controllers.js
+++ backend/controllers/users-controllers.js
@@ -1,5 +1,7 @@
 const { v4: uuidv4 } = require('uuid');
 
+const { validationResult } = require('express-validator');
+
 const HttpError = require('../models/http-error');
 
 const DUMMY_USERS = [
@@ -16,6 +18,10 @@ const getUsers = (req, res, next) => {
 };
 
 const signup = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    throw new HttpError('Invalid inputs passed, please check your data.', 422);
+  }
   const { name, email, password } = req.body;
 
   const hasUser = DUMMY_USERS.find(u => u.email === email);
--- backend/routes/users-routes.js
+++ backend/routes/users-routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { check } = require('express-validator');
 
 const usersController = require('../controllers/users-controllers');
 
@@ -6,7 +7,19 @@ const router = express.Router();
 
 router.get('/', usersController.getUsers);
 
-router.post('/signup', usersController.signup);
+router.post(
+  '/signup',
+  [
+    check('name')
+      .not()
+      .isEmpty(),
+    check('email')
+      .normalizeEmail() // Test@test.com => test@test.com
+      .isEmail(),
+    check('password').isLength({ min: 6 })
+  ],
+  usersController.signup
+);
 
 router.post('/login', usersController.login);
 
