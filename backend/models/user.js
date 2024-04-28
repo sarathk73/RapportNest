269e8209c10e5938e99656b10338a772f885f447
SARATH K <142706927+sarathk73@users.noreply.github.com>
2024-04-28 09:55:55 +0530

    Add User schema with unique email validation

---
+++ backend/models/user.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+const Schema = mongoose.Schema;
+
+
+const userSchema = new Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true, minlength: 6 },
+    image: { type: String, required: true },
+    contacts: { type: String, required: true },
+});
+
+userSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model('User', userSchema);
+
