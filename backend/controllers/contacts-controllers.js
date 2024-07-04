4a6ad2b0496d0793f92906ae70770d6200894d9f
SARATH K <142706927+sarathk73@users.noreply.github.com>
2024-07-04 13:03:57 +0530

    Minor Bug fixes and improved error logging

--- backend/controllers/contacts-controllers.js
+++ backend/controllers/contacts-controllers.js
@@ -156,36 +156,32 @@ const getContactsByUserId = async (req, res, next) => {
     let contact;
     try {
       contact = await Contact.findById(contactId).populate('creator');
+      if (!contact) {
+        const error = new HttpError('Could not find contact for this id.', 404);
+        return next(error);
+      }
     } catch (err) {
-      const error = new HttpError(
-        'Something went wrong, could not delete contact.',
-        500
-      );
-      return next(error);
-    }
-  
-    if (!contact) {
-      const error = new HttpError('Could not find contact for this id.', 404);
+      console.error(`Error fetching contact with ID ${contactId}: `, err); 
+      const error = new HttpError('Something went wrong, could not delete contact.', 500);
       return next(error);
     }
   
     try {
       const sess = await mongoose.startSession();
       sess.startTransaction();
-      await contact.remove({ session: sess });
+      await contact.deleteOne({ session: sess }); 
       contact.creator.contacts.pull(contact);
       await contact.creator.save({ session: sess });
       await sess.commitTransaction();
     } catch (err) {
-      const error = new HttpError(
-        'Something went wrong, could not delete contact.',
-        500
-      );
+      console.error(`Error deleting contact with ID ${contactId}: `, err); 
+      const error = new HttpError('Something went wrong, could not delete contact.', 500);
       return next(error);
     }
   
     res.status(200).json({ message: 'Deleted contact.' });
   };
+  
 exports.getContactById = getContactById;
 exports.getContactsByUserId = getContactsByUserId; 
 exports.createContact = createContact;
