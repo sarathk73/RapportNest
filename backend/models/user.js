9748e89c87c5d964e8fe485aeab04651d5e268dc
SARATH K <142706927+sarathk73@users.noreply.github.com>
2024-07-07 17:57:16 +0530

    Refactor: user schema to include additional fields required for user profile information

--- backend/models/user.js
+++ backend/models/user.js
@@ -3,11 +3,15 @@ const uniqueValidator = require('mongoose-unique-validator');
 
 const Schema = mongoose.Schema;
 
-
 const userSchema = new Schema({
-    name: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true, minlength: 6 },
+    dateOfBirth: { type: Date, required: true },
+    gender: { type: String, required: true },
+    phoneNumbers: [{ type: String, required: true }],
+    address: { type: String, required: true },
     image: { type: String, required: true },
     contacts: [{ type: mongoose.Types.ObjectId, required: true, ref: 'Contact'}]
 });
@@ -15,4 +19,3 @@ const userSchema = new Schema({
 userSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model('User', userSchema);
-
