876c54047c2ce3fab6fe815563670077487085b7
SARATH K <142706927+sarathk73@users.noreply.github.com>
2024-07-09 10:58:05 +0530

    feat: Add additional validators and enhance validation logic

--- frontend/src/shared/util/validators.js
+++ frontend/src/shared/util/validators.js
@@ -5,6 +5,8 @@ const VALIDATOR_TYPE_MIN = 'MIN';
 const VALIDATOR_TYPE_MAX = 'MAX';
 const VALIDATOR_TYPE_EMAIL = 'EMAIL';
 const VALIDATOR_TYPE_FILE = 'FILE';
+const VALIDATOR_TYPE_PHONE = 'PHONE';
+const VALIDATOR_TYPE_MATCH = 'MATCH';
 
 export const VALIDATOR_REQUIRE = () => ({ type: VALIDATOR_TYPE_REQUIRE });
 export const VALIDATOR_FILE = () => ({ type: VALIDATOR_TYPE_FILE });
@@ -19,6 +21,8 @@ export const VALIDATOR_MAXLENGTH = val => ({
 export const VALIDATOR_MIN = val => ({ type: VALIDATOR_TYPE_MIN, val: val });
 export const VALIDATOR_MAX = val => ({ type: VALIDATOR_TYPE_MAX, val: val });
 export const VALIDATOR_EMAIL = () => ({ type: VALIDATOR_TYPE_EMAIL });
+export const VALIDATOR_PHONE = () => ({ type: VALIDATOR_TYPE_PHONE });
+export const VALIDATOR_MATCH = val => ({ type: VALIDATOR_TYPE_MATCH, val: val });
 
 export const validate = (value, validators) => {
   let isValid = true;
@@ -40,6 +44,18 @@ export const validate = (value, validators) => {
     }
     if (validator.type === VALIDATOR_TYPE_EMAIL) {
       isValid = isValid && /^\S+@\S+\.\S+$/.test(value);
+    }
+    if (validator.type === VALIDATOR_TYPE_PHONE) {
+      if (Array.isArray(value)) {
+        for (const phoneNumber of value) {
+          isValid = isValid && /^[0-9]{10}$/.test(phoneNumber.trim());
+        }
+      } else {
+        isValid = isValid && /^[0-9]{10}$/.test(value.trim());
+      }
+    }
+    if (validator.type === VALIDATOR_TYPE_MATCH) {
+      isValid = isValid && value === validator.val;
     }
   }
   return isValid;
