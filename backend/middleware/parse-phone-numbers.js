5cfb3b5917bdd637b43123d7a8c76a417bc21960
SARATH K <142706927+sarathk73@users.noreply.github.com>
2024-07-22 12:41:59 +0530

    Enhance phone numbers parsing middleware with validation

--- backend/middleware/parse-phone-numbers.js
+++ backend/middleware/parse-phone-numbers.js
@@ -1,15 +1,26 @@
+const { parsePhoneNumberFromString } = require('libphonenumber-js');
+
 const parsePhoneNumbers = (req, res, next) => {
-    if (req.body.phoneNumbers) {
-      try {
-        console.log('Before Parsing:', req.body.phoneNumbers);
-        req.body.phoneNumbers = JSON.parse(req.body.phoneNumbers);
-        console.log('After Parsing:', req.body.phoneNumbers);
-      } catch (err) {
-        return res.status(400).json({ message: 'Invalid phoneNumbers format' });
-      }
+  if (req.body.phoneNumbers) {
+    try {
+      console.log('Before Parsing:', req.body.phoneNumbers);
+      req.body.phoneNumbers = JSON.parse(req.body.phoneNumbers);
+      console.log('After Parsing:', req.body.phoneNumbers);
+
+      // Validate and format phone numbers
+      req.body.phoneNumbers = req.body.phoneNumbers.map(phoneNumber => {
+        const phoneNumberObj = parsePhoneNumberFromString(phoneNumber, 'IN'); // Adjust default region if necessary
+        if (!phoneNumberObj || !phoneNumberObj.isValid()) {
+          throw new Error('Invalid phone number format.');
+        }
+        return phoneNumberObj.number; // Use normalized number
+      });
+
+    } catch (err) {
+      return res.status(400).json({ message: 'Invalid phoneNumber format,Check the Digits' });
     }
-    next();
-  };
-  
-  module.exports = parsePhoneNumbers;
-  
+  }
+  next();
+};
+
+module.exports = parsePhoneNumbers;
