b0edae223d3e1ebd3f143090369247464e5d8c1f
SARATH K <142706927+sarathk73@users.noreply.github.com>
2024-07-22 12:50:46 +0530

    Enhance contact routes with new features and middleware

--- backend/routes/contacts-routes.js
+++ backend/routes/contacts-routes.js
@@ -1,45 +1,46 @@
 const express = require('express');
-
 const { check } = require('express-validator');
 
 const contactsControllers = require('../controllers/contacts-controllers');
 const fileUpload = require('../middleware/file-upload');
 const checkAuth = require('../middleware/check-auth');
+const parsePhoneNumber = require('../middleware/parse-phone-number');
 
 const router = express.Router();
 
-router.get('/:pid', contactsControllers.getContactById );
+router.get('/search', contactsControllers.searchContacts);
+
+router.get('/:pid', contactsControllers.getContactById);
 
 router.get('/user/:uid', contactsControllers.getContactsByUserId);
+
+router.get('/user/:uid/paginated', contactsControllers.getContactsByUserIdPaginated);
+
+
 
 router.use(checkAuth);
 
 router.post(
   '/',
   fileUpload.single('image'),
-    [
-    check('title')
-      .not()
-      .isEmpty(),
+  parsePhoneNumber,
+  [
+    check('title').not().isEmpty(),
     check('description').isLength({ min: 5 }),
-    check('phone')
-      .not()
-      .isEmpty()
-    ],
-    contactsControllers.createContact
+    check('phone').not().isEmpty()
+  ],
+  contactsControllers.createContact
 );
 
-router.patch('/:pid',
-    [
-      check('title')
-        .not()
-        .isEmpty(),
-      check('description').isLength({ min: 5 }),
-      check('phone')
-      .not()
-      .isEmpty()
-    ],
-    contactsControllers.updateContact
+router.patch(
+  '/:pid',
+  parsePhoneNumber,
+  [
+    check('title').not().isEmpty(),
+    check('description').isLength({ min: 5 }),
+    check('phone').not().isEmpty()
+  ],
+  contactsControllers.updateContact
 );
 
 router.delete('/:pid', contactsControllers.deleteContact);
