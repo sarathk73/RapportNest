885e9057ad04c84f74e409b81d20bbc6b3f88476
SARATH K <142706927+sarathk73@users.noreply.github.com>
2024-07-24 21:38:51 +0530

    Align and organize codes

--- backend/controllers/contacts-controllers.js
+++ backend/controllers/contacts-controllers.js
@@ -1,62 +1,42 @@
 const fs = require('fs');
-
 const { validationResult } = require('express-validator');
 const mongoose = require('mongoose');
-
 const HttpError = require('../models/http-error');
 const Contact = require('../models/contact');
 const User = require('../models/user');
 
 const DEFAULT_IMAGE_URL = 'uploads/images/no-user.jpg';
 
-
 const getContactById = async (req, res, next) => {
-  const contactId = req.params.pid; 
+  const contactId = req.params.pid;
 
   let contact;
   try {
     contact = await Contact.findById(contactId);
   } catch (err) {
-    const error = new HttpError(
-      'Something went wrong, could not find a contact.',
-      500
-    );
-    return next(error);
+    return next(new HttpError('Something went wrong, could not find a contact.', 500));
   }
 
   if (!contact) {
-    const error = new HttpError(
-      'Could not find a contact for the provided id.',
-      404
-    );
-    return next(error);
+    return next(new HttpError('Could not find a contact for the provided id.', 404));
   }
 
-  res.json({ contact: contact.toObject({ getters: true }) }); 
+  res.json({ contact: contact.toObject({ getters: true }) });
 };
-
 
 const getContactsByUserId = async (req, res, next) => {
   const userId = req.params.uid;
 
-  
   let userWithContacts;
   try {
     userWithContacts = await User.findById(userId).populate('contacts');
   } catch (err) {
-    console.error(`Error fetching contacts for user ${userId}: `, err); 
-    const error = new HttpError(
-      'Fetching Contacts failed, please try again later',
-      500
-    );
-    return next(error);
+    console.error(`Error fetching contacts for user ${userId}: `, err);
+    return next(new HttpError('Fetching Contacts failed, please try again later', 500));
   }
 
-  
   if (!userWithContacts || userWithContacts.contacts.length === 0) {
-    return next(
-      new HttpError('Could not find contacts for the provided user id.', 404)
-    );
+    return next(new HttpError('Could not find contacts for the provided user id.', 404));
   }
 
   res.json({ contacts: userWithContacts.contacts.map(contact => contact.toObject({ getters: true })) });
@@ -69,15 +49,9 @@ const createContact = async (req, res, next) => {
   }
 
   const { title, description, phone, tags } = req.body;
+  const imagePath = req.file ? req.file.path : DEFAULT_IMAGE_URL;
 
-  let imagePath;
-  if (req.file) {
-    imagePath = req.file.path;
-  } else {
-    imagePath = DEFAULT_IMAGE_URL;
-  }
-
-  console.log('Received Tags:', tags); 
+  console.log('Received Tags:', tags);
   const parsedTags = JSON.parse(tags);
 
   const createdContact = new Contact({
@@ -93,13 +67,11 @@ const createContact = async (req, res, next) => {
   try {
     user = await User.findById(req.userData.userId);
   } catch (err) {
-    const error = new HttpError('Creating contact failed, please try again.', 500);
-    return next(error);
+    return next(new HttpError('Creating contact failed, please try again.', 500));
   }
 
   if (!user) {
-    const error = new HttpError('Could not find user for provided id.', 404);
-    return next(error);
+    return next(new HttpError('Could not find user for provided id.', 404));
   }
 
   try {
@@ -110,22 +82,16 @@ const createContact = async (req, res, next) => {
     await user.save({ session: sess });
     await sess.commitTransaction();
   } catch (err) {
-    const error = new HttpError('Creating contact failed, please try again.', 500);
-    return next(error);
+    return next(new HttpError('Creating contact failed, please try again.', 500));
   }
 
   res.status(201).json({ contact: createdContact });
 };
 
-
-
-
 const updateContact = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return next(
-      new HttpError('Invalid inputs passed, please check your data.', 422)
-    );
+    return next(new HttpError('Invalid inputs passed, please check your data.', 422));
   }
 
   const { title, description, phone, tags } = req.body;
@@ -135,19 +101,11 @@ const updateContact = async (req, res, next) => {
   try {
     contact = await Contact.findById(contactId);
   } catch (err) {
-    const error = new HttpError(
-      'Something went wrong, could not update contact.',
-      500
-    );
-    return next(error);
+    return next(new HttpError('Something went wrong, could not update contact.', 500));
   }
 
   if (contact.creator.toString() !== req.userData.userId) {
-    const error = new HttpError(
-      'You are not allowed to edit this contact.',
-      401
-    );
-    return next(error);
+    return next(new HttpError('You are not allowed to edit this contact.', 401));
   }
 
   contact.title = title;
@@ -158,123 +116,107 @@ const updateContact = async (req, res, next) => {
   try {
     await contact.save();
   } catch (err) {
-    const error = new HttpError(
-      'Something went wrong, could not update contact.',
-      500
-    );
-    return next(error);
+    return next(new HttpError('Something went wrong, could not update contact.', 500));
   }
 
   res.status(200).json({ contact: contact.toObject({ getters: true }) });
 };
 
-  
+const deleteContact = async (req, res, next) => {
+  const contactId = req.params.pid;
 
-  const deleteContact = async (req, res, next) => {
-    const contactId = req.params.pid;
-  
-    let contact;
-    try {
-      contact = await Contact.findById(contactId).populate('creator');
-      if (!contact) {
-        const error = new HttpError('Could not find contact for this id.', 404);
-        return next(error);
-      }
-    } catch (err) {
-      console.error(`Error fetching contact with ID ${contactId}: `, err); 
-      const error = new HttpError('Something went wrong, could not delete contact.', 500);
-      return next(error);
+  let contact;
+  try {
+    contact = await Contact.findById(contactId).populate('creator');
+    if (!contact) {
+      return next(new HttpError('Could not find contact for this id.', 404));
     }
+  } catch (err) {
+    console.error(`Error fetching contact with ID ${contactId}: `, err);
+    return next(new HttpError('Something went wrong, could not delete contact.', 500));
+  }
 
-    if (contact.creator.id !== req.userData.userId) {
-      const error = new HttpError(
-        'You are not allowed to delete this contact.',
-        401
-      );
-      return next(error);
-    }
+  if (contact.creator.id !== req.userData.userId) {
+    return next(new HttpError('You are not allowed to delete this contact.', 401));
+  }
 
-    const imagePath = contact.image;
-  
-    try {
-      const sess = await mongoose.startSession();
-      sess.startTransaction();
-      await contact.deleteOne({ session: sess }); 
-      contact.creator.contacts.pull(contact);
-      await contact.creator.save({ session: sess });
-      await sess.commitTransaction();
-    } catch (err) {
-      console.error(`Error deleting contact with ID ${contactId}: `, err); 
-      const error = new HttpError('Something went wrong, could not delete contact.', 500);
-      return next(error);
-    }
+  const imagePath = contact.image;
 
+  try {
+    const sess = await mongoose.startSession();
+    sess.startTransaction();
+    await contact.deleteOne({ session: sess });
+    contact.creator.contacts.pull(contact);
+    await contact.creator.save({ session: sess });
+    await sess.commitTransaction();
+  } catch (err) {
+    console.error(`Error deleting contact with ID ${contactId}: `, err);
+    return next(new HttpError('Something went wrong, could not delete contact.', 500));
+  }
 
+  if (imagePath !== DEFAULT_IMAGE_URL) {
     fs.unlink(imagePath, err => {
-      console.log(err);
+      if (err) {
+        console.error(`Error deleting image file at path ${imagePath}: `, err);
+      } else {
+        console.log(`Successfully deleted image file at path ${imagePath}`);
+      }
     });
-  
-    res.status(200).json({ message: 'Deleted contact.' });
-  };
+  }
 
-  const getContactsByUserIdPaginated = async (req, res, next) => {
-    const userId = req.params.uid;
-    const page = parseInt(req.query.page) || 1;
-    const limit = parseInt(req.query.limit) || 3;
-  
-    let contacts;
-    try {
-      contacts = await Contact.find({ creator: userId })
-        .skip((page - 1) * limit)
-        .limit(limit);
-    } catch (err) {
-      const error = new HttpError(
-        'Fetching contacts failed, please try again later.',
-        500
-      );
-      return next(error);
-    }
-  
-    const totalContacts = await Contact.countDocuments({ creator: userId });
-  
-    res.json({
-      contacts: contacts.map(contact => contact.toObject({ getters: true })),
-      currentPage: page,
-      totalPages: Math.ceil(totalContacts / limit)
-    });
-  };
-  
-  const searchContacts = async (req, res, next) => {
-    const { name } = req.query;
-    const page = parseInt(req.query.page) || 1;
-    const limit = parseInt(req.query.limit) || 3;
-
-    let contacts;
-    try {
-        
-        contacts = await Contact.find({
-            title: new RegExp(name, 'i')
-        })
-        .skip((page - 1) * limit)
-        .limit(limit);
-    } catch (err) {
-        const error = new HttpError(
-            'Fetching contacts failed, please try again later.',
-            500
-        );
-        return next(error);
-    }
-
-    const totalContacts = await Contact.countDocuments({
-        title: new RegExp(name, 'i')
-    });
-
-    res.json({
-        contacts: contacts.map(contact => contact.toObject({ getters: true })),
-        currentPage: page,
-        totalPages: Math.ceil(totalContacts / limit)
-    });
+  res.status(200).json({ message: 'Deleted contact.' });
 };
+
+const getContactsByUserIdPaginated = async (req, res, next) => {
+  const userId = req.params.uid;
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 3;
+
+  let contacts;
+  try {
+    contacts = await Contact.find({ creator: userId })
+      .skip((page - 1) * limit)
+      .limit(limit);
+  } catch (err) {
+    return next(new HttpError('Fetching contacts failed, please try again later.', 500));
+  }
+
+  const totalContacts = await Contact.countDocuments({ creator: userId });
+
+  res.json({
+    contacts: contacts.map(contact => contact.toObject({ getters: true })),
+    currentPage: page,
+    totalPages: Math.ceil(totalContacts / limit)
+  });
+};
+
+const searchContacts = async (req, res, next) => {
+  const { name } = req.query;
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 3;
+
+  let contacts;
+  try {
+    contacts = await Contact.find({
+      title: new RegExp(name, 'i')
+    })
+      .skip((page - 1) * limit)
+      .limit(limit);
+  } catch (err) {
+    return next(new HttpError('Fetching contacts failed, please try again later.', 500));
+  }
+
+  const totalContacts = await Contact.countDocuments({
+    title: new RegExp(name, 'i')
+  });
+
+  res.json({
+    contacts: contacts.map(contact => contact.toObject({ getters: true })),
+    currentPage: page,
+    totalPages: Math.ceil(totalContacts / limit)
+  });
+};
+
 const getAllTags = async (req, res, next) => {
   try {
     const tags = await Contact.aggregate([
@@ -284,11 +226,7 @@ const getAllTags = async (req, res, next) => {
     ]);
     res.json({ tags });
   } catch (err) {
-    const error = new HttpError(
-      "Fetching tags failed, please try again later.",
-      500
-    );
-    return next(error);
+    return next(new HttpError('Fetching tags failed, please try again later.', 500));
   }
 };
 
@@ -303,11 +241,7 @@ const getContactsByTag = async (req, res, next) => {
       .skip((page - 1) * limit)
       .limit(limit);
   } catch (err) {
-    const error = new HttpError(
-      "Fetching contacts failed, please try again later.",
-      500
-    );
-    return next(error);
+    return next(new HttpError('Fetching contacts failed, please try again later.', 500));
   }
 
   const totalContacts = await Contact.countDocuments({ tags: tag });
@@ -319,9 +253,8 @@ const getContactsByTag = async (req, res, next) => {
   });
 };
 
-  
 exports.getContactById = getContactById;
-exports.getContactsByUserId = getContactsByUserId; 
+exports.getContactsByUserId = getContactsByUserId;
 exports.createContact = createContact;
 exports.updateContact = updateContact;
 exports.deleteContact = deleteContact;
